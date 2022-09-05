e82e832d1073e7748a7fa7ede18ca900b17ee750
asifamanapon <aponasifaman@gmail.com>
2022-09-05 21:55:40 +0600

    fourth-commit

--- grade.js
+++ grade.js
@@ -21,3 +21,80 @@ else if (marks1 >= 40) {
 else {
     console.log('Grade F');
 }
+// for second person 
+if (marks2 >= 80 ) {
+    console.log('Garde A');
+
+}
+else if (marks2 >= 60 ) {
+    console.log('Grade B');
+
+}
+else if (marks2 >= 50 ) {
+    console.log('Grade C');
+
+}
+else if (marks2 >= 40) {
+    console.log('Grade D');
+}
+else {
+    console.log('Grade F');
+    
+}
+// for third person 
+if (marks3 >= 80 ) {
+    console.log('Garde A');
+
+}
+else if (marks3 >= 60 ) {
+    console.log('Grade B');
+
+}
+else if (marks3>= 50 ) {
+    console.log('Grade C');
+
+}
+else if (marks3 >= 40) {
+    console.log('Grade D');
+}
+else {
+    console.log('Grade F');
+}
+// for forth person 
+if (marks4 >= 80 ) {
+    console.log('Garde A');
+
+}
+else if (marks4 >= 60 ) {
+    console.log('Grade B');
+
+}
+else if (marks4 >= 50 ) {
+    console.log('Grade C');
+
+}
+else if (marks4 >= 40) {
+    console.log('Grade D');
+}
+else {
+    console.log('Grade F');
+}
+// for fifth person 
+if (marks5 >= 80 ) {
+    console.log('Garde A');
+
+}
+else if (marks5 >= 60 ) {
+    console.log('Grade B');
+
+}
+else if (marks5 >= 50 ) {
+    console.log('Grade C');
+
+}
+else if (marks5 >= 40) {
+    console.log('Grade D');
+}
+else {
+    console.log('Grade F');
+}
